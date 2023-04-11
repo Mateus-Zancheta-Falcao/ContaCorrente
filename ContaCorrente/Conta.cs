c94a618cb4f2617dd0daaee7a58c5c7649515931
Mateus Zancheta Falcao <mateuszancheta2005@gmail.com>
2023-04-11 11:49:29 -0300

    Add project files.

---
+++ ContaCorrente/Conta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente
+{
+    internal class Conta
+    {
+        public int numero { get; set; }
+        public decimal saldo { get; set; }
+        public decimal limite { get; set; }
+        public bool ehEspecial { get; set; }
+        public Movimentacao[] movimentacoes;
+
+        public decimal Sacar(decimal sacar)
+        {
+            Movimentacao movimentacao = new Movimentacao();
+
+            int posicaoVazia = PegaPosicaoVazia();
+
+            movimentacoes[posicaoVazia] = movimentacao;
+
+            return saldo = saldo - sacar;
+        }
+
+        public decimal Depositar(decimal depositar)
+        {
+            Movimentacao movimentacao = new Movimentacao();
+
+            int posicaoVazia = PegaPosicaoVazia();
+
+            movimentacoes[posicaoVazia] = movimentacao;
+
+            return saldo += depositar;
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine($"Extrato - Saldo: {saldo}");
+        }
+
+
+        public decimal TransferirPara(Conta conta, decimal deposito)
+        {
+            Sacar(deposito);
+
+            decimal valorDepositado = conta.Depositar(deposito);
+
+            return valorDepositado;
+        }
+
+        public int PegaPosicaoVazia()
+        {
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                if (movimentacoes[i] == null)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ ContaCorrente/Movimentacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente
+{
+    internal class Movimentacao
+    {
+        public string descricao;
+
+        public double valor;
+
+        public string tipo;
+    }
+}
